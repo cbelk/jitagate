ff2c1261a5539f6a1a973814f6a5592503bbad08
Christian Belk <christian@acst.com>
2025-07-14 14:03:50 -0400

    added functions to get a list of tailscale groups and generate a modal for slack

--- jitagate.py
+++ jitagate.py
@@ -6,6 +6,9 @@ import json
 import os
 import requests
 
+# Get the approvers for the given tailscale group
+#   ddb_approvers_table:    the name of the approvers table
+#   ts_group:               the tailscale group to get the approvers for (partition key for DynamoDB table)
 def get_approvers(ddb_approvers_table, ts_group):
     client = boto3.client('dynamodb')
     try:
@@ -16,6 +19,132 @@ def get_approvers(ddb_approvers_table, ts_group):
     else:
         #return json.loads(res['Item'])
         return res['Item']['approvers']
+
+# Get a list of tailscale groups. This is generated from the partition keys of the approvers table in DynamoDB
+#   ddb_approvers_table: the name of the approvers table
+def get_groups(ddb_approvers_table):
+    client = boto3.client('dynamodb')
+    try:
+        res = client.scan(TableName = ddb_approvers_table, ProjectionExpression = 'ts_group')
+    except botocore.exceptions.ClientError as e:
+        ##### Better error handling here #####
+        raise Exception(e)
+    else:
+        groups = []
+        for item in res['Items']:
+            groups.append(item['ts_group']['S'])
+        return groups
+
+# Generate the modal to present the user in slack
+#   trigger_id: the trigger_id generated when the user issued the slash command (3 second lifespan)
+#   groups:     the list of tailscale groups to present to the user
+def get_request_modal(trigger_id, groups):
+    view = {'type': 'modal',
+            'submit': {
+                'type': 'plain_text',
+                'text': 'Submit',
+                'emoji': True
+            },
+            'close': {
+                'type': 'plain_text',
+                'text': 'Cancel',
+                'emoji': True
+            },
+            'title': {
+                'type': 'plain_text',
+                'text': 'Jitagate',
+                'emoji': True
+            },
+            'blocks': [
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text': 'Access details:'
+                    }
+                },
+                {
+                    'type': 'divider'
+                },
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text': ':clipboard: *Group*\nChoose which group you need access to'
+                    },
+                    'accessory': {
+                        'type': 'static_select',
+                        'placeholder': {
+                            'type': 'plain_text',
+                            'text': 'Group list',
+                            'emoji': True
+                        },
+                        'options': []
+                    }
+                },
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text': ':clock1: *Duration*\nHow long do you need access'
+                    },
+                    'accessory': {
+                        'type': 'static_select',
+                        'placeholder': {
+                            'type': 'plain_text',
+                            'text': 'duration',
+                            'emoji': True
+                        },
+                        'options': [
+                            {
+                                'text': {
+                                    'type': 'plain_text',
+                                    'text': '*30 min*',
+                                    'emoji': True
+                                },
+                                'value': '1800'
+                            },
+                            {
+                                'text': {
+                                    'type': 'plain_text',
+                                    'text': '*1 hour*',
+                                    'emoji': True
+                                },
+                                'value': '3600'
+                            },
+                            {
+                                'text': {
+                                    'type': 'plain_text',
+                                    'text': '*6 hours*',
+                                    'emoji': True
+                                },
+                                'value': '21600'
+                            },
+                            {
+                                'text': {
+                                    'type': 'plain_text',
+                                    'text': '*12 hours*',
+                                    'emoji': True
+                                },
+                                'value': '43200'
+                            }
+                        ],
+                        'action_id': 'static_select-action'
+                    }
+                }
+            ]
+    }
+    for group in groups:
+        option = {
+                    'text': {
+                        'type': 'plain_text',
+                        'text': '*{}*'.format(group),
+                        'emoji': True
+                    },
+                    'value': group
+                 }
+        view['blocks'][2]['accessory']['options'].append(option)
+    return {'trigger_id': trigger_id, 'view': view}
 
 # Get the secret string from secrets manager
 def get_secret(secret_name):
@@ -29,6 +158,7 @@ def get_secret(secret_name):
         return json.loads(secret_response['SecretString'])
 
 # Get an API key from tailscale using the oauth client
+#   oauth_client:  the OAuth2 client to use to generate the API token
 def get_tailscale_api_token(oauth_client):
     url = 'https://api.tailscale.com/api/v2/oauth/token'
     data = {
@@ -39,6 +169,8 @@ def get_tailscale_api_token(oauth_client):
     return res.json()
 
 # Send message to Slack webhook
+#   slack_webhook:  the slack webhook to send the message to
+#   message:        the message to send
 def send_slack_message(slack_webhook, message):
     res = requests.post(slack_webhook, data=json.dumps(message))
     attempts = 1
@@ -76,12 +208,13 @@ def main(event={}, context={}):
     slack_webhook = secret_string['webhook']
     secret_string = get_secret(ts_secret_name)
     ts_api_token = get_tailscale_api_token(secret_string)['access_token']
-    policy = tailscale_api_get(ts_api_token, tailnet, 'acl')
-    ts_users = tailscale_api_get(ts_api_token, tailnet, 'users')
-    approvers = get_approvers(ddb_approvers_table, 'testers')
-    print(policy)
-    print(ts_users)
-    print(approvers)
+#    policy = tailscale_api_get(ts_api_token, tailnet, 'acl')
+#    ts_users = tailscale_api_get(ts_api_token, tailnet, 'users')
+#    approvers = get_approvers(ddb_approvers_table, 'testers')
+    groups = get_groups(ddb_approvers_table)
+    modal = get_request_modal('my_trigger_id', groups)
+    print(groups)
+    print(json.dumps(modal))
 
 if __name__ == '__main__':
     main()
